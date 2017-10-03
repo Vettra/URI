37f90260e5c98819baa7f58bd238e20f4e98b53d
Vettra <32470937+Vettra@users.noreply.github.com>
2017-10-03 05:57:16 -0300

    Create 1014.cpp

---
+++ 1014.cpp
@@ -0,0 +1,37 @@
+///////////////////////////////////
+//////Maratona de Programação//////
+////Supervisor: Douglas Cardoso////
+///////////////////////////////////
+//////URI JUGDE: Nível básico//////
+//////////Exercício: 1012//////////
+//////////Gustavo B. Ruiz//////////
+///////////////////////////////////
+ 
+///////////////////////////////////
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <locale>
+#include <map>
+#include <math.h>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+///////////////////////////////////
+ 
+int main()
+{
+ 
+    //////////VÁRIAVEIS//////////
+        double a, b, t;
+    /////////////////////////////
+    scanf("%lf %lf", &a, &b);//%lf PARA DOUBLE
+    
+    
+    t = a/b; //SEMPRE CONTAR DOUBLE COM DOUBLE, LEMBRE-SE!!
+    
+    printf("%.3lf  km/l", t);//%.3lf PARA 3 CASAS DECIMAIS
+    return 0;
+}
