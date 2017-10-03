746087862c10001c03cf42690f4dc69fdf9abaf6
Vettra <32470937+Vettra@users.noreply.github.com>
2017-10-03 00:01:27 -0300

    Create 1011.cpp

---
+++ 1011.cpp
@@ -0,0 +1,37 @@
+///////////////////////////////////
+//////Maratona de Programação//////
+////Supervisor: Douglas Cardoso////
+///////////////////////////////////
+//////URI JUGDE: Nível básico//////
+//////////Exercício: 1011//////////
+//////////Gustavo B. Ruiz//////////
+///////////////////////////////////
+
+///////////////////////////////////
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <locale>
+#include <map>
+#include <math.h>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+///////////////////////////////////
+ 
+int main()
+{
+    
+    //////////VÁRIAVEIS//////////
+        double pi=3.14159, r, v;
+    /////////////////////////////
+    
+    scanf("%lf", &r);//%lf PARA DOUBLE
+    
+    v = (4/3) * pi * (r^3);// "^" É UM RECURSO UTILIZADO PARA ELEVAR UM NÚMERO À "X", NO CASO RAIO ELEVADO À 3
+    
+    printf("VVOLUME =  %.3lf\n", t);//3.lf PARA APENAS 3 CASAS DECIMAIS
+    return 0;
+}
