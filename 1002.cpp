3c8e1750f719b9652e8c3aea4d1f0891dd5b029e
Vettra <32470937+Vettra@users.noreply.github.com>
2017-10-02 21:26:50 -0300

    Create 1002.cpp

---
+++ 1002.cpp
@@ -0,0 +1,32 @@
+////////////////////////////////////////
+//////////////PROBLEM 1002//////////////
+///////////////BY:VETTRA////////////////
+////////////////////////////////////////
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <locale>
+#include <map>
+#include <math.h>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+ 
+using namespace std;
+ 
+int main()
+{
+    
+    //////////VÁRIAVEIS//////////
+        double r, a, pi=3.14159;
+    /////////////////////////////
+    
+    scanf("%lf", &r);
+    
+    a = pi*(r*r);
+ 
+    printf("A = %lf", a);
+    return 0;
+}
