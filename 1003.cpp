e78237253a18f82a31e282b5458089e8bc82cb05
Vettra <32470937+Vettra@users.noreply.github.com>
2017-10-02 21:38:27 -0300

    Create 1003.cpp

---
+++ 1003.cpp
@@ -0,0 +1,32 @@
+////////////////////////////////////////
+//////////////PROBLEM 1003//////////////
+///////////////BY:VETTRA////////////////
+////////////////////////////////////////
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <locale>
+#include <map>
+#include <math.h>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+ 
+using namespace std;
+ 
+int main()
+{
+    
+    //////////VÁRIAVEIS//////////
+            int a, b, c;
+    /////////////////////////////
+    
+    scanf("%d %d", &a, &b);
+    
+    c = (a + b);
+ 
+    printf("SOMA = %d\n", c);
+    return 0;
+}
