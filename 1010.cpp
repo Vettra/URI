b1d425bec226ae0b673437dc1a6e841570af5fbf
Vettra <32470937+Vettra@users.noreply.github.com>
2017-10-02 23:27:46 -0300

    Create 1010.cpp

---
+++ 1010.cpp
@@ -0,0 +1,33 @@
+////////////////////////////////////////
+//////////////PROBLEM 1010//////////////
+///////////////BY:VETTRA////////////////
+////////////////////////////////////////
+//////////////BIBLIOTECAS///////////////
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <locale>
+#include <map>
+#include <math.h>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+///////////////////////////////////////// 
+ 
+int main()
+{
+    
+    //////////VÁRIAVEIS//////////
+            int a, a2;
+            double b, c, b2, c2, t;
+    /////////////////////////////
+    
+    scanf("%d %lf %lf\n %d %lf %lf", &a, &b, &c, &a2, &b2, &c2);//A ORDENAÇÃO DAS VARIÁVEIS NESTE PROBLEMA É FUNDAMENTAL
+    
+    t = (b*c) + (b2*c2);// O PROBLEMA PEDE A ENTRADA DA UNIDADE COMO VALOR INTEIRO, MAS LEMBRE DOS EXERCÍCIOS ANTERIORES, EVITE FAZER CONTAS DOUBLE COM INT, TRANSFORME TUDO EM DOUBLE
+ 
+    printf("VALOR A PAGAR: R$ %.2f\n", t);//2.lf PARA APENAS 2 CASAS DECIMAIS
+    return 0;
+}
