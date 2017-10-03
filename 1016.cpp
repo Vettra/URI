6f31e579d760081adcef2bbda45f32f65dcb1594
Vettra <32470937+Vettra@users.noreply.github.com>
2017-10-03 06:11:57 -0300

    Create 1016.cpp

---
+++ 1016.cpp
@@ -0,0 +1,37 @@
+///////////////////////////////////
+//////Maratona de Programação//////
+////Supervisor: Douglas Cardoso////
+///////////////////////////////////
+//////URI JUGDE: Nível básico//////
+//////////Exercício: 1012//////////
+//////////Gustavo B. Ruiz//////////
+///////////////////////////////////
+ 
+///////////////////////////////////
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <locale>
+#include <map>
+#include <math.h>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+///////////////////////////////////
+ 
+int main()
+{
+ 
+    //////////VÁRIAVEIS//////////
+        int x;
+    /////////////////////////////
+    scanf("%d", &x);//%d PARA INT
+    
+    
+    x = x*2;//ESPERO QUE ATÉ AQUI VOCÊ CONSIGA FORMULAR ALGO TÃO SIMPLES!! SZ
+    
+    printf("%d minutos\n", x);//ESPERO QUE ATÉ AQUI VOCÊ NÃO ESQUEÇA O \N!! LOL
+    return 0;
+}
