c7cc39ca1470203b77520c110268a5b13b460c2f
Vettra <32470937+Vettra@users.noreply.github.com>
2017-10-02 22:18:04 -0300

    Create 1006.cpp

---
+++ 1006.cpp
@@ -0,0 +1,35 @@
+////////////////////////////////////////
+//////////////PROBLEM 1006//////////////
+///////////////BY:VETTRA////////////////
+////////////////////////////////////////
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <locale>
+#include <map>
+#include <math.h>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+ 
+using namespace std;
+ 
+int main()
+{
+    
+    //////////VÁRIAVEIS//////////
+            double a, b, c, t;
+    /////////////////////////////
+    
+    scanf("%lf %lf %lf", &a, &b, &c);
+    
+    a = a*2;//MULTIPLICAR PELO PESO DA NOTA A
+    b = b*3;//MULTIPLICAR PELO PESO DA NOTA B
+    c = c*5;//MULTIPLICAR PELO PESO DA NOTA C
+    t = (a+b+c)/10;//DIVIDIR PELO PESO TOTAL A SOMA DAS NOTAS
+ 
+    printf("MEDIA = %.1lf\n", t);//%."NUMERO"lf, NUMERO são as casas decimais que você quer
+    return 0;
+}
