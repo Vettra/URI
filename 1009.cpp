0631dc9e5f6874bd7ca62b990612bdda0c8639dd
Vettra <32470937+Vettra@users.noreply.github.com>
2017-10-02 23:13:15 -0300

    Create 1009.cpp

---
+++ 1009.cpp
@@ -0,0 +1,33 @@
+////////////////////////////////////////
+//////////////PROBLEM 1007//////////////
+///////////////BY:VETTRA////////////////
+////////////////////////////////////////
+//////////////BIBLIOTECAS///////////////
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <locale>
+#include <map>
+#include <math.h>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+///////////////////////////////////////// 
+ 
+int main()
+{
+    
+    //////////VÁRIAVEIS//////////
+            char nome;
+            double a, b, t;
+    /////////////////////////////
+    
+    scanf("%s %lf %lf", &nome, &a, &b);//%s para variável char. guarda "letras"
+    
+    t = a + (b*0.15);//FORMULA PARA PAGAMENTO DO FUNCIONÁRIO(SALÁRIO FIXO + 15% DE COMISSÃO)
+ 
+    printf("TOTAL = R$ %.2f\n", t);//2.lf PARA APENAS 2 CASAS DECIMAIS
+    return 0;
+}
