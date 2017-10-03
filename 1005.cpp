ee43733c98f3f31294ace30c41d7e2427b4bbb1f
Vettra <32470937+Vettra@users.noreply.github.com>
2017-10-02 22:14:08 -0300

    Create 1005.cpp

---
+++ 1005.cpp
@@ -0,0 +1,34 @@
+////////////////////////////////////////
+//////////////PROBLEM 1005//////////////
+///////////////BY:VETTRA////////////////
+////////////////////////////////////////
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <locale>
+#include <map>
+#include <math.h>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+ 
+using namespace std;
+ 
+int main()
+{
+    
+    //////////VÁRIAVEIS//////////
+            double a, b, c;
+    /////////////////////////////
+    
+    scanf("%lf %lf", &a, &b);
+    
+    a = a*3.5;//MULTIPLICAR PELO PESO DA NOTA A
+    b = b*7.5;//MULTIPLICAR PELO PESO DA NOTA B
+    c = (a+b)/11;//DIVIDIR PELO PESO TOTAL A SOMA DAS NOTAS
+ 
+    printf("MEDIA = %.5lf\n", c);//%."NUMERO"lf, NUMERO são as casas decimais que você quer
+    return 0;
+}
