9391d1dc7cabc23df0b9977bd471d006832c2600
Vettra <32470937+Vettra@users.noreply.github.com>
2017-10-02 21:17:42 -0300

    Create 1001.cpp

---
+++ 1001.cpp
@@ -0,0 +1,32 @@
+////////////////////////////////////////
+//////////////PROBLEM 1001//////////////
+///////////////BY:VETTRA////////////////
+////////////////////////////////////////
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <locale>
+#include <map>
+#include <math.h>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+ 
+using namespace std;
+ 
+int main()
+{
+    
+    //////////VÁRIAVEIS//////////
+            int a, b, c;
+    /////////////////////////////
+    
+    scanf("%d %d", &a, &b);
+    
+    c = (a + b);
+ 
+    printf("X = %d\n", c);
+    return 0;
+}
