c9a7a77ac70d410d6a73294494ab2fa77767b4ba
Vettra <32470937+Vettra@users.noreply.github.com>
2017-10-02 22:53:47 -0300

    Update 1008.cpp

--- 1008.cpp
+++ 1008.cpp
@@ -26,8 +26,8 @@ int main()
     
     scanf("%d %lf %lf", &a, &b, &c);
     
-    t = b*c;
+    t = b*c;//EVITE FAZER CONTAS COM VÁRIAVEIS INT E DOUBLE, CASO O RESULTADO FINAL TENHA CASAS DECIMAIS TRANSFORME TODAS VARIÁVEIS EM DOUBLE.
  
-    printf("NUMBER = %d\nSALARY = U$ %.2lf\n", a, t);
+    printf("NUMBER = %d\nSALARY = U$ %.2lf\n", a, t);//ORDENE AS VARIÁVEIS DEPOIS DAS (" ",) NESTE CASO ''A'' PRIMEIRO ''T'' DEPOIS
     return 0;
 }
