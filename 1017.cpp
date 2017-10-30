5fa33a4b85b1891703599b4050af8c905084fe9e
Gustavo Ruiz <32470937+Vettra@users.noreply.github.com>
2017-10-30 18:53:17 -0200

    Create 1017.cpp

---
+++ 1017.cpp
@@ -0,0 +1,37 @@
+///////////////////////////////////
+//////Maratona de Programação//////
+////Supervisor: Douglas Cardoso////
+///////////////////////////////////
+//////URI JUGDE: Nível básico//////
+//////////Exercício: 1017//////////
+//////////Gustavo B. Ruiz//////////
+///////////////////////////////////
+ 
+///////////////////////////////////
+#include <algorithm>
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <locale>
+#include <map>
+#include <math.h>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+///////////////////////////////////
+ 
+int main()
+{
+ 
+    //////////VÁRIAVEIS//////////
+        double x, y;
+    /////////////////////////////
+    scanf("%lf %lf", &x y);//%lf PARA DOUBLE, RESULTADO FINAL EM DECIMAL
+    
+    
+    x = (x * y) / 12;//ESPERO QUE ATÉ AQUI VOCÊ CONSIGA FORMULAR ALGO TÃO SIMPLES!! SZ
+    
+    printf("%.3lf \n", x);//ESPERO QUE ATÉ AQUI VOCÊ NÃO ESQUEÇA O \N!! LOL
+    return 0;
+}
